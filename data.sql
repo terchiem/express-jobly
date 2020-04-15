79507e82cc965e08d2fb8a9a083efa7cedc4ca04
Graham Trail <Graham@Grahams-MacBook-Pro.local>
2020-04-15 16:28:24 -0700

    Started on jobs

--- data.sql
+++ data.sql
@@ -6,3 +6,11 @@ CREATE TABLE companies (
     logo_url text
 );
 
+CREATE TABLE jobs (
+    id serial PRIMARY KEY,
+    title text NOT NULL,
+    salary float NOT NULL,
+    equity float NOT NULL CHECK (equity >= 0 AND equity <= 1),
+    company_handle text NOT NULL REFERENCES companies(handle) ON DELETE CASCADE,
+    date_posted timestamp with time zone DEFAULT NOW()    
+);
---
+++ models/jobs.js
@@ -0,0 +1,165 @@
+const db = require("../db");
+const ExpressError = require("../helpers/expressError");
+const sqlForPartialUpdate = require("../helpers/partialUpdate")
+
+
+class Job {
+
+  /**
+   * Returns all jobs:
+   * [{title, company_handle}, ...] ordered by time posted
+   */
+  static async all() {
+    const result = await db.query(`
+      SELECT title,
+             company_handle
+             FROM jobs
+      ORDER BY date_posted DESC`);
+
+    return result.rows;
+  }
+
+  /** 
+   * Returns one job by id, with the full company data object:
+   * {title, salary, equity, company: {handle, name, num_employees, description, logo_url}, date_posted}
+   */
+
+  static async get(id) {
+    const result = await db.query(`
+      SELECT title,
+             salary,
+             equity,
+             company_handle,
+             date_posted,
+             companies.name,
+             companies.num_employees,
+             companies.description,
+             companies.logo_url
+             FROM jobs
+             WHERE id=$1
+      JOIN companies
+      ON jobs.company_handle = companies.handle`,
+      [id]);
+
+    if (result.rows.length === 0) {
+      throw new ExpressError(`Cannot find job for ${id}`, 404);
+    }
+    const d = result.rows[0];
+
+    const jobObj = { 
+      title: d.title,
+      salary: d.salary,
+      equity: d.equity,
+      company: {handle: d.company_handle,
+                name: d.name,
+                num_employees: d.num_employees,
+                description: d.description,
+                logo_url: d.logo_url}, 
+      date_posted: d.date_posted
+    }
+
+    return jobObj;
+  }
+
+  /**
+   * Create a new job, accepts an object:
+   * {title, salary, equity, company_handle}
+   * 
+   * Returns the new job:
+   * {id, title, salary, equity, company_handle, date_posted}
+   */
+
+  static async create({ title, salary, equity, company_handle }) { 
+    try {
+    // create new job
+    const result = await db.query(`
+      INSERT INTO jobs (
+        title, 
+        salary, 
+        equity, 
+        company_handle)
+        VALUES ($1, $2, $3, $4)
+        RETURNING id, title, salary, equity, company_handle, date_posted`,
+      [title, salary, equity, company_handle]);
+
+    return result.rows[0];
+    } catch(err){
+      throw new ExpressError("Error creating new job.", 400)
+    }
+  }
+
+  /**
+   * Update an existing company, accepts an object:
+   * {handle, name, num_employees, description, logo_url}
+   * 
+   * Returns the result of the update
+   * {handle, name, num_employees, description, logo_url}
+   */
+
+  static async update(items, handle) {
+    const sql = sqlForPartialUpdate("companies", items, "handle", handle);
+
+    const result = await db.query(sql.query, sql.values);
+
+    if (result.rows.length === 0) {
+      throw new ExpressError(`Cannot find company for ${handle}`, 404);
+    }
+
+    return result.rows[0];
+  }
+
+  /**
+   * Delete a company, accepts a string:
+   * handle
+   * 
+   * Returns the deleted company:
+   * {handle, name, num_employees, description, logo_url}
+   */
+
+  static async delete(handle) {
+    const result = await db.query(`
+      DELETE from companies
+      WHERE handle=$1
+      RETURNING 
+        handle,
+        name,
+        num_employees,
+        description,
+        logo_url`,
+      [handle])
+
+    if (result.rows.length === 0) {
+      throw new ExpressError(`Cannot find company for ${handle}`, 404);
+    }
+    
+    return result.rows[0];
+   }
+
+
+  /**
+   * Search for companies by search term that have a number of employees
+   * between min and max. (max defaults to largest value possible for sql integers)
+   * 
+   * Returns an array of companies:
+   * [{handle, name, num_employees, description, logo_url}, ...]
+   * 
+   */
+
+  static async getByQueries(searchTerm="", min=0, max=2147483647) {
+    const result = await db.query(`
+      SELECT handle,
+             name,
+             num_employees,
+             description,
+             logo_url
+             FROM companies
+             WHERE (handle ILIKE $1 OR name ILIKE $1) 
+              AND num_employees BETWEEN $2 AND $3`, 
+             [`%${searchTerm}%`, min, max]);        // ways to not include all WHERE clauses
+
+    return result.rows;
+  }
+}
+
+
+module.exports = Company;
